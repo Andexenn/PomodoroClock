a82df2bb868928b4436f5f6af6c281055607c5b2
ryan <phanhuynhhung0707@gmail.com>
2025-07-04 21:46:02 +0700

    3. basic to do list HTML and CSS

---
+++ main.js
@@ -0,0 +1,9 @@
+const modes = document.querySelectorAll("[data-time]");
+const timer = document.querySelector(".timer");
+const streak = document.querySelector(".streak");
+
+function populateTime() {
+
+}
+
+modes.forEach((mode) => mode.addEventListener("click", ));
